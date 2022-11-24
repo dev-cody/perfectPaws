c9f049310e5a8b73b124969a5a00088b628d91f8
Cody Koscielski <codykoscielski@gmail.com>
2022-11-24 14:18:41 -0500

    Setup React using Vite
    Started to setup React for the client frontend using Vite.

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+
+import AdminDash from './pages/admin/index'
+import Navbar from './components/ui/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <AdminDash />
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/admin/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDash = () => {
+  return (
+    <div>Hi</div>
+  )
+}
+
+export default AdminDash
